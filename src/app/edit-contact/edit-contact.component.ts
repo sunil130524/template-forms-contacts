b6f0cf3f8332eff35d461a283fe7c47b1e8ec02c
Sunil Kumar C <sunil130524@gmail.com>
2024-11-09 07:39:26 +0530

    Creating a angular template form

--- src/app/edit-contact/edit-contact.component.ts
+++ src/app/edit-contact/edit-contact.component.ts
@@ -1,22 +1,51 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { Contact } from '../contacts/contact.model';
+import { ContactsService } from '../contacts/contacts.service';
 
 @Component({
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   standalone: true,
   templateUrl: './edit-contact.component.html',
   styleUrls: ['./edit-contact.component.css']
 })
 export class EditContactComponent implements OnInit {
-  constructor(private route: ActivatedRoute) { }
+  contact: Contact = {
+    id: '',
+    firstName: '',
+    lastName: '',
+    dateOfBirth: null,
+    favoritesRanking: 0,
+    phone: {
+      phoneNumber: '',
+      phoneType: '',
+    },
+    address: {
+      streetAddress: '',
+      city: '',
+      state: '',
+      postalCode: '',
+      addressType: '',
+    },
+  }
+  constructor(private route: ActivatedRoute, private contactsService: ContactsService, private router: Router) { }
 
   ngOnInit() {
     const contactId = this.route.snapshot.params['id'];
     if (!contactId) return
+    this.contactsService.getContact(contactId).subscribe((contact) => {
+      if(contact) {
+        this.contact = contact;
+      }
+    })
   }
 
   saveContact() {
-
+    console.log(this.contact);
+    this.contactsService.saveContact(this.contact).subscribe(() => {
+      this.router.navigate(['./contacts']);
+    })
   }
 }
